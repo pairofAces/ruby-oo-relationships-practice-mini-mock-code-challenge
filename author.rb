1846c91ac6a481e31b853373a97e61300905830b
pairofAces <karanchauhan16@gmail.com>
2020-07-20 10:27:35 -0400

    Mod1 Mock Challenge

--- author.rb
+++ author.rb
@@ -1,1 +1,46 @@
+class Author
+    attr_reader :name
+    @@all = []
 
+    def initialize(name)
+        @name = name
+        Author.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def books
+        Book.all.select do |book|
+            book.author == self
+        end
+    end
+
+    def write_book(title, word_count)
+        Book.new(title, self, word_count)
+    end
+
+    def total_words
+        total = 0
+        books.map do |book|
+            total += book.word_count
+        end
+        total
+    end
+
+    def self.most_words
+        most = 0
+        top_writer = ""
+        self.all.each do |author|
+            words = author.total_words
+            if words > most
+                most = words
+                top_writer = author.name
+            end
+        end
+        top_writer
+    end
+
+
+end
--- console.rb
+++ console.rb
@@ -2,6 +2,19 @@ require 'pry'
 require_relative './book'
 require_relative './author'
 
+karan = Author.new("Karan S. Chauhan")
+nazia = Author.new("Nazia Talukder")
+ragnar = Author.new("Ragnar Lothbrok")
+
+
+karan.write_book("autobiography", 1000)
+nazia.write_book("graduates", 2000)
+ragnar.write_book("Power is given to those who are prepared to lower themselves to pick it up", 5000)
+ragnar.write_book("I wish I Never Left The Farm", 2000)
+ragnar.write_book("Power", 10000)
+
+
+
 binding.pry
 
 
